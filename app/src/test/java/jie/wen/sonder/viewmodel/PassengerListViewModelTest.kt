67a140f4b9d3d7e0fd579e282de48c3d8d9a8652
Wen <jie.wen@live.hk>
2021-08-16 10:20:22 +0800

    update unit test

--- app/src/test/java/jie/wen/sonder/viewmodel/PassengerListViewModelTest.kt
+++ app/src/test/java/jie/wen/sonder/viewmodel/PassengerListViewModelTest.kt
@@ -8,6 +8,7 @@ import jie.wen.sonder.repository.PassengerDataRepository
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
+import okhttp3.ResponseBody
 import org.junit.*
 import retrofit2.Response
 
@@ -21,11 +22,13 @@ class PassengerListViewModelTest {
 
     @ExperimentalCoroutinesApi
     @Test
-    fun testFetchPassengerListData() {
+    fun testFetchPassengerListDataSuccess() {
         val repository : PassengerDataRepository = mockk()
         val response = PassengerListResponseDTO()
-        val success = Response.success(response)
+        val success = Response.success(200, response)
         val passengerListViewModel = PassengerListViewModel(repository)
+        // success case
+        passengerListViewModel.currentPage.set(0)
         coEvery { repository.fetchPassengerListData(0) } returns success
         testDispatcher.runBlockingTest {
             passengerListViewModel.fetchPassengerListData(0)
@@ -34,6 +37,27 @@ class PassengerListViewModelTest {
         }
 
         coVerify(exactly = 1) { repository.fetchPassengerListData(0) }
+    }
 
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testFetchPassengerListDataFail() {
+        val repository : PassengerDataRepository = mockk()
+        val response = PassengerListResponseDTO()
+        val errorResponse : ResponseBody = ResponseBody.create(null, ByteArray(0))
+        val passengerListViewModel = PassengerListViewModel(repository)
+        val failResponse = Response.error<PassengerListResponseDTO>(400, errorResponse)
+
+        // failed case
+        passengerListViewModel.currentPage.set(0)
+
+        coEvery { repository.fetchPassengerListData(0) } returns failResponse
+        testDispatcher.runBlockingTest {
+            passengerListViewModel.fetchPassengerListData(0)
+            Assert.assertEquals(passengerListViewModel.currentPage.get(), 0)
+            Assert.assertEquals(Resource.error(errorResponse.toString(), null), passengerListViewModel.passengerListResponseDTOLiveData.value)
+        }
+
+        coVerify(exactly = 1) { repository.fetchPassengerListData(0) }
     }
 }
